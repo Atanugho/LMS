e3f5922981b562d81179be674e03d8f5a4493df0
Atanugho <ghoshatanu395@gmail.com>
2024-07-09 15:45:56 +0530

    add aboutus page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import HomePage from './Pages/HomePage';
 import Footer from './Components/Footer';
+import AboutUs from './Pages/AboutUs';
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/about" element={<AboutUs />}></Route>
       </Routes>
     </>
   )
--- client/src/Layouts/HomeLayout.jsx
+++ client/src/Layouts/HomeLayout.jsx
@@ -1,9 +1,19 @@
 import {FiMenu} from 'react-icons/fi';
 import {AiFillCloseCircle} from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../Components/Footer';
+import {useDispatch, useSelector} from 'react-redux';
+
 
 function HomeLayout({ children }) {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+    const role = useSelector((state) => state?.auth?.role);
+
 
 
     function changewidth(){
@@ -17,6 +27,15 @@ function HomeLayout({ children }) {
 
         const drawerSide = document.getElementsByClassName("drawer-side");
         drawerSide[0].style.width = '0';
+    }
+
+    function handleLogout(e){
+        e.preventDefault();
+
+        //const res = await dispatch(logout());
+
+        //if(res?.payload?.success)
+        navigate("/")
     }
 
 
@@ -36,15 +55,23 @@ function HomeLayout({ children }) {
                 <div className="drawer-side w-0">
                     <label htmlFor='my-drawer' className='drawer-overlay'>     
                     </label>
-                    <ul className="menu p-4 w-48 sm:w-80 bg-base-100 text-base-content relative">
+                    <ul className="menu p-4 w-48 h-[100%] sm:w-80 bg-base-100 text-base-content relative">
                         <li className='w-fit absolute right-2 z-50'>
                             <button onClick={hideDrawer}>
                                 <AiFillCloseCircle size={24} />
                             </button>
                         </li>
+                    
                         <li>
                             <Link to="/">Home</Link>
                         </li>
+
+                        {isLoggedIn && role === 'ADMIN' && (
+                            <li>
+                                <Link to="/admin/dashboard"> Admin DashBoard</Link>
+                            </li>
+                        )}
+
                         <li>
                             <Link to="/courses">All Courses</Link>
                         </li>
@@ -54,6 +81,32 @@ function HomeLayout({ children }) {
                         <li>
                             <Link to="/about">About Us</Link>
                         </li>
+
+                        {!isLoggedIn && (
+                            <li className='absolute bottom-4 [w-90%]'>
+                                <div className='w-full flex items-center justify-center'>
+                                    <button className='  bg-blue-800 px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to="/login"> Login </Link>
+                                    </button>
+                                    <button className='bg-pink-600  px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to="/signup"> Signup </Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
+
+                        {isLoggedIn && (
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className='btn-primary px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to="/user/profile">Profile</Link>
+                                    </button>
+                                    <button className='btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link onClick={handleLogout}>Logout</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
