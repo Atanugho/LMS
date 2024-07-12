65fb02ea6282855dec1356ac4314c1fb3efe4325
Atanugho <ghoshatanu395@gmail.com>
2024-07-12 21:40:07 +0530

    Update axiosInstance.js and add regexMatcher.js  and create the signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import HomePage from './Pages/HomePage';
-import Footer from './Components/Footer';
 import AboutUs from './Pages/AboutUs';
 import NotFound from './Pages/NotFound';
+import Signup from './Pages/Signup';
 
 function App() {
   
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
-
+        <Route path="/signup" element={<Signup />}></Route>
 
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
