c0be98690b2542d2dd7435f2f2bf0a71d46de1a6
Atanugho <ghoshatanu395@gmail.com>
2024-07-16 21:54:08 +0530

    added sign in functionality

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -5,6 +5,7 @@ import asyncHandler from "./asyncHandler.middleware.js";
 export const isLoggedIn = asyncHandler(async (req, _res, next) => {
   
   const { token } = req.cookies;
+  
   if (!token) {
     return next(new AppError("Unauthorized, please login to continue", 401));
   }
