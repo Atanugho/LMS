58478b78a72185dd872ac92465c6f47b38ef32df
Atanugho <ghoshatanu395@gmail.com>
2024-07-21 15:49:18 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import CourseList from './Pages/Course/CourseList';
 import Contact from './Pages/Contact';
 import Denied from './Pages/Denied';
 import CourseDescription from './Pages/Course/CourseDescription';
+import RequireAuth from './Components/auth/RequireAuth';
+import CreateCourse from './Pages/Course/CreateCourse';
 
 function App() {
   
@@ -20,11 +22,18 @@ function App() {
         <Route path="/courses" element={<CourseList />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/denied" element={<Denied />}></Route>
+
+
         <Route path="/course/description" element={<CourseDescription />}></Route>
 
 
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/login" element={<Login />}></Route>
+
+        <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
+            <Route path="/course/create" element={<CreateCourse />}></Route>
+        </Route>
+
 
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
