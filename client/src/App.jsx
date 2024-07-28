7b0a04379548ced47e3c14e7ff9421904403905a
Atanugho <ghoshatanu395@gmail.com>
2024-07-28 16:17:57 +0530

    added lectures upload functionality by admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,6 +17,7 @@ import Checkout from './Pages/Payment/Checkout';
 import CheckoutSuccess from './Pages/Payment/CheckoutSuccess';
 import CheckoutFailure from './Pages/Payment/CheckoutFailure';
 import Displaylectures from './Pages/Dashboard/DisplayLectures';
+import AddLecture from './Pages/Dashboard/AddLecture';
 
 function App() {
   
@@ -38,6 +39,7 @@ function App() {
 
         <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
             <Route path="/course/create" element={<CreateCourse />}></Route>
+            <Route path="/course/addlecture" element={<AddLecture />} />
         </Route>
 
         <Route element={<RequireAuth allowedRoles={["ADMIN","USER"]} />}>
